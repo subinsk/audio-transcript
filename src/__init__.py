7bff0bee9f282aa02923b8cab13b13fe68a564c3
Subin S K <subinsk284@gmail.com>
2025-08-20 18:23:56 +0530

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+"""
+Audio Transcriber and Summarizer Package
+"""
+
+__version__ = "1.0.0"
+__author__ = "Audio Transcription Team"
+
+from .batch_processor import BatchProcessor
+from .config import *
+
+__all__ = ['BatchProcessor']
